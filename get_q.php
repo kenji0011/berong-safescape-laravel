3918c0ca8c46c85264f941e95ea7dcd9b593b7a9
Kean <keangabriel101@gmail.com>
2026-04-26 11:08:40 +0800

    Cleanup: Remove some from tracking

--- get_q.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$questions = \App\Models\AssessmentQuestion::where('type', 'preTest')
-    ->whereJsonContains('forRoles', 'adult')
-    ->select('id', 'question', 'order')
-    ->get();
-file_put_contents('q_out.json', json_encode($questions, JSON_PRETTY_PRINT));
